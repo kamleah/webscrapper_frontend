c9ac27603f45b4dd3b94afd912e18b6009b4b2aa
Swati Dasi <swatidasi919@gmail.com>
2025-03-04 16:48:36 +0530

    merge

--- src/constants/BaseConfig.jsx
+++ src/constants/BaseConfig.jsx
@@ -1,8 +1,8 @@
 // export const baseURL = `http://127.0.0.1:8000/`;
-export const baseURL = `http://192.168.0.181:8000/`;
+// export const baseURL = `http://192.168.0.181:8000/`;
 // export const baseURL = `http://127.0.0.1:8000/`;
 // export const baseURL = `http://192.168.0.181:8000/`;
 // export const baseURL = `http://169.254.36.146:8000/`;
 // export const baseURL = `https://api.siverce.com/`;
 // export const baseURL = `https://api.siverce.com/`;
-// export const baseURL = `https://scrapper-api.techiebears.com/`;
+export const baseURL = `https://scrapper-api.techiebears.com/`;
