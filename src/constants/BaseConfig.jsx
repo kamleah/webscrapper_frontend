e188417e4c405f0860c330f84b4a321207990cdc
Kamlesh Gupta <kamleshgupta594@gmail.com>
2025-04-02 16:25:25 +0530

    added live url

--- src/constants/BaseConfig.jsx
+++ src/constants/BaseConfig.jsx
@@ -1,3 +1,3 @@
-export const baseURL = `http://127.0.0.1:8000/`;
+// export const baseURL = `http://127.0.0.1:8000/`;
 // export const baseURL = `http://192.168.0.181:8000/`; 
-// export const baseURL = `https://scrapper-api.techiebears.com/`;
+export const baseURL = `https://scrapper-api.techiebears.com/`;
